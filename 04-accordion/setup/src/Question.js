df213db036796c5c7446505c1d839f113f3c70e6
engAbdullahHaif <engabdullahhanif@gmail.com>
2022-06-21 14:45:58 +0430

    accordion project completed

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({title, info}) => {
+  const [showInfo, sentShowInfo] = useState(false)
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => {sentShowInfo(!showInfo)}}>
+          {showInfo ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      {showInfo && info}
+    </article>
+    );
 };
 
 export default Question;
